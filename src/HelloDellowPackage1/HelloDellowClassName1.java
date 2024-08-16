17d46eb8a3dbdac321a66b64d101f8966b5832fc
Wendell Lopez Guevarra <wendelllopezguevarra@gmail.com>
2024-08-16 21:54:12 +0800

    Commit Message: initial commit1

---
+++ src/HelloDellowPackage1/HelloDellowClassName1.java
@@ -0,0 +1,10 @@
+package HelloDellowPackage1;
+
+public class HelloDellowClassName1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow Dellow!1");
+	}
+
+}
